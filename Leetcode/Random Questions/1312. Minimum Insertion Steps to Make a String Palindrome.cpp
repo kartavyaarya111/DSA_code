3c1be052871e8a61339cdf27fb32d00b502ecfbd
Kartavya Arya <kartavyaarya111@gmail.com>
2024-08-31 16:22:42 +0530

    Created Random question folder

---
+++ Leetcode/Random Questions/1312. Minimum Insertion Steps to Make a String Palindrome.cpp
@@ -0,0 +1,26 @@
+//Revise this only in this sequence
+// LCS -> LPS -> then this question
+
+class Solution {
+public:
+    int minInsertions(string s1) {
+        string s2=s1;
+        reverse(s2.begin(),s2.end());
+        int n = s1.length();
+        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+        for(int i=0;i<=n;i++){
+            dp[i][0]=0;
+            dp[0][i]=0;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(s1[i-1]==s2[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }else{
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return n-dp[n][n];
+    }
+};
