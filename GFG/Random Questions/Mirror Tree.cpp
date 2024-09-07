86e62985a67c862835104e760f70f83b7f836af2
Kartavya Arya <kartavyaarya111@gmail.com>
2024-09-08 01:39:14 +0530

    Mirror Tree

---
+++ GFG/Random Questions/Mirror Tree.cpp
@@ -0,0 +1,28 @@
+//EASY Questions
+
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child /
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+}; */
+
+class Solution {
+  public:
+    // Function to convert a binary tree into its mirror tree.
+    void mirror(Node* node) {
+        if(node==NULL)  return;
+        Node* tmp = node->left;
+        node->left=node->right;
+        node->right=tmp;
+        mirror(node->left);
+        mirror(node->right);
+    }
+};
